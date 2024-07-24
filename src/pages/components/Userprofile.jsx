abf57a2edf31980238c6ef0f964a568e73620aeb
unknown <developmentbyali@gmail.com>
2024-07-24 19:40:16 +0500

    revert the profile changes

--- src/pages/components/Userprofile.jsx
+++ src/pages/components/Userprofile.jsx
@@ -6,8 +6,6 @@ const userprofile = () => {
       
 
 
-
-      
       <div class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
         <div class="flex justify-end px-4 pt-4">
           {/* <button
